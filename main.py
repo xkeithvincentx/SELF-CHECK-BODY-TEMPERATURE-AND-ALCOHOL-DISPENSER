3d1323b1409a47f5201b85df955bba10dbeb2f16
Keith <keithvincent283@usjr.edu.ph>
2021-05-27 13:58:05 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import RPi.GPIO as GPIO
 import time as sleep
 import tm1637 as DSPSEG
 import drivers as DSPLCD
 import adafruit_mlx90614 as MLX
 import board
+from mfrc522 import SimpleMFRC522
 
+
+reader = SimpleMFRC522()
 segmentDisplay = DSPSEG.TM1637(clk=4, dio=17)
 tempSensor = MLX.MLX90614(board.I2C())
 
@@ -13,8 +17,13 @@ def i2c_lcd(temperature):
     lcdDisplay.lcd_display_string(str(temperature), 2)  # Write line of text to second line of display
 
 
+def read_rfid():
+    id = reader.read()
+    return id
+
 while 1:
     temperature = tempSensor.object_temperature
     segmentDisplay.temperature(temperature)
     i2c_lcd(temperature)
+    id = read_rfid()
     sleep(1)
