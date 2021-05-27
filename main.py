5d02cdb43a9b79b27e544fc2be59c71ff43fa776
Chingotipep Keith Rañoa <61351023+xkeithvincentx@users.noreply.github.com>
2021-05-27 09:36:27 +0800

    Update main.py
    BASIC SAMPLE CODE

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import RPi.GPIO as GPIO  
 import time as sleep
+import tm1637 as DSPSEG
+import drivers as DSPLCD
+import adafruit_mlx90614 as MLX
+import board
+
+segmentDisplay = DSPSEG.TM1637(clk=4, dio=17)
+tempSensor = MLX.MLX90614(board.I2C())
+
+def i2c_lcd(temperature):
+    lcdDisplay = DSPLCD.lcd()
+    lcdDisplay.lcd_display_string("=TEMPERATURE=", 1)  # Write line of text to first line of display
+    lcdDisplay.lcd_display_string(str(temperature), 2)  # Write line of text to second line of display
+
+
+while 1:
+    temperature = tempSensor.object_temperature
+    segmentDisplay.temperature(temperature)
+    i2c_lcd(temperature)
+    sleep(1)
